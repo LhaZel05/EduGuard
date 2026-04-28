5a009d8d96d918c0270584ae3338ad3423b19959
Lha Zel Wangchuk <12240048.gcit@rub.edu.bt>
2026-04-28 23:43:53 +0600

    Create App.js as the main entry point for the Expo app, importing and rendering the CreateClassDetails component.

---
+++ App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { View } from 'react-native';
+import CreateClassDetails from './CreateClassDetails';
+
+const App = () => {
+  return (
+    <View style={{ flex: 1 }}>
+      <CreateClassDetails />
+    </View>
+  );
+};
+
+export default App;
