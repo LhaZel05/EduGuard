662c2682aafed8e4c56b9d8182c663b3f56158ec
Lha Zel Wangchuk <12240048.gcit@rub.edu.bt>
2026-04-29 00:01:53 +0600

    Create CreateClassDetails.js with React component converted from TSX

---
+++ CreateClassDetails.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './CreateClassDetails.css';
+
+const CreateClassDetails = () => {
+    return (
+        <div className="create-class-details">
+            <h1>Create Class Details</h1>
+            {/* Add your form or components here */}
+        </div>
+    );
+};
+
+export default CreateClassDetails;
