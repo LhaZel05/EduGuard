0f48009692eccf3990910f23d4503c3681f28095
Lha Zel Wangchuk <12240048.gcit@rub.edu.bt>
2026-04-29 00:05:44 +0600

    Create MonitorList.js React component from TSX

---
+++ MonitorList.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const MonitorList = () => {
+    // Your existing logic here
+    return (
+        <div>
+            {/* Your existing JSX here */}
+        </div>
+    );
+};
+
+export default MonitorList;
